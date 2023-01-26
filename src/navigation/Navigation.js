954f81caf28fe1ac4e81ca4c95308094e5c2b67d
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-01-26 16:23:45 -0500

    Add :: Designing the Tab navigation with icons

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 // Screens
@@ -10,14 +11,64 @@ const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Favorite" component={Favorite} options={options} />
-      <Tab.Screen name="Pokedex" component={Pokedex} options={options} />
-      <Tab.Screen name="Account" component={Account} options={options} />
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+      }}
+      screenOptions={{
+        tabBarStyle: {
+          position: "absolute",
+          height: 90,
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Favorite"
+        component={Favorite}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => {
+            return (
+              <Image
+                source={require("../assets/corazon.png")}
+                style={{ width: 40, height: 40 }}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Pokedex"
+        component={Pokedex}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => {
+            return (
+              <Image
+                source={require("../assets/pokebola.png")}
+                style={{ width: 70, height: 70 }}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={Account}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => {
+            return (
+              <Image
+                source={require("../assets/jugador.png")}
+                style={{ width: 40, height: 40 }}
+              />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
-
-const options = {
-  headerShown: false,
-};
