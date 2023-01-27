216756752a1a09b967b721f5246ed85087dc0040
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-01-27 14:54:23 -0500

    Add :: Consumo de PokeApi

---
+++ src/api/pokemon.js
@@ -0,0 +1,12 @@
+import { API_HOST } from "../utils/constants";
+
+export async function getPokemonsApi() {
+  try {
+    const url = "${API_HOST}/pokemon?limit=20&offset=0";
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,11 +1,29 @@
 import { Text, SafeAreaView, Button } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getPokemonsApi } from "../api/pokemon";
 
 export default function Pokedex({ navigation }) {
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
-      <Button title="Go to Pokemon" onPress={() => navigation.navigate('Pokemon')} />
+      <Button
+        title="Go to Pokemon"
+        onPress={() => navigation.navigate("Pokemon")}
+      />
     </SafeAreaView>
   );
 }
