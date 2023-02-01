6d8d3b914931d3f9e432aee5b2fa8ddc3c31c3c5
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-02-01 17:07:26 -0500

    Add :: Information of all Pokemon

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -2,7 +2,17 @@ import { API_HOST } from "../utils/constants";
 
 export async function getPokemonsApi() {
   try {
-    const url = "${API_HOST}/pokemon?limit=20&offset=0";
+    const url = `${API_HOST}/pokemon?limit=20&offset=0`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getPokemonDetailsByUrlApi(url) {
+  try {
     const response = await fetch(url);
     const data = await response.json();
     return data;
--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,8 +1,11 @@
 import { Text, SafeAreaView, Button } from "react-native";
 import React, { useState, useEffect } from "react";
-import { getPokemonsApi } from "../api/pokemon";
+import { getPokemonsApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
 
 export default function Pokedex({ navigation }) {
+  const [pokemons, setPokemons] = useState([]);
+  console.log(pokemons);
+
   useEffect(() => {
     (async () => {
       await loadPokemons();
@@ -11,7 +14,19 @@ export default function Pokedex({ navigation }) {
 
   const loadPokemons = async () => {
     try {
-      const response = await getPokemonsApi();
+      const data = await getPokemonsApi();
+      const pokemonsArray = [];
+      for await (const pokemon of data.results) {
+        const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url);
+        pokemonsArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.types[0],
+          order: pokemonDetails.order,
+          image: pokemonDetails.sprites.other["official-artwork"].front_default,
+        });
+      }
+      setPokemons([...pokemons, ...pokemonsArray]);
     } catch (error) {
       console.error(error);
     }
